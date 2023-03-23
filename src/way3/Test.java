0c60384f06d66c8e77c53684eb158d603f6ccbf4
Dmytro Chupryna <dmytro.chupryna18@gmail.com>
2023-03-23 20:55:18 +0200

    changed a test class

--- src/way3/Test.java
+++ src/way3/Test.java
@@ -4,21 +4,21 @@ import java.util.Collections;
 
 public class Test {
     public static void main(String[] args) {
-        Library library = new Library("Central library of KhNU V.N.Karazina");
+        Library library = new Library("Library");
 
-        Bookshelf bookshelf1 = new Bookshelf("Science");
+        Bookshelf bookshelf1 = new Bookshelf("Programming");
         Bookshelf bookshelf2 = new Bookshelf("History");
-        Bookshelf bookshelf3 = new Bookshelf("Chemistry");
+        Bookshelf bookshelf3 = new Bookshelf("Science");
 
-        Book b1 = new Book("T1", new Author("Name1", "Surname1"), 1996);
-        Book b2 = new Book("T2", new Author("Name2", "Surname2"), 2011);
-        Book b3 = new Book("T3", new Author("Name3", "Surname3"), 2014);
-        Book b4 = new Book("T4", new Author("Name4", "Surname4"), 1995);
-        Book b5 = new Book("T5", new Author("Name5", "Surname5"), 1965);
-        Book b6 = new Book("T6", new Author("Name6", "Surname6"), 2007);
-        Book b7 = new Book("T7", new Author("Name7", "Surname7"), 1969);
-        Book b8 = new Book("T8", new Author("Name8", "Surname8"), 2018);
-        Book b9 = new Book("T10", new Author("Name9", "Surname9"), 1999);
+        Book b1 = new Book("book1", new Author("Name1", "Surname1"), 2000);
+        Book b2 = new Book("book2", new Author("Name2", "Surname2"), 2023);
+        Book b3 = new Book("book3", new Author("Name3", "Surname3"), 1999);
+        Book b4 = new Book("book4", new Author("Name4", "Surname4"), 1923);
+        Book b5 = new Book("book5", new Author("Name5", "Surname5"), 1900);
+        Book b6 = new Book("book6", new Author("Name6", "Surname6"), 1999);
+        Book b7 = new Book("book7", new Author("Name7", "Surname7"), 1990);
+        Book b8 = new Book("book8", new Author("Name8", "Surname8"), 2011);
+        Book b9 = new Book("book9", new Author("Name9", "Surname9"), 2014);
 
         bookshelf1.addBook(b1);
         bookshelf1.addBook(b2);
@@ -38,8 +38,8 @@ public class Test {
         library.addBookshelf(bookshelf2);
         library.addBookshelf(bookshelf3);
 
-        Reader bookReader1 = new Reader("NameBR1", "SurnameBR1", 33);
-        Reader bookReader2 = new Reader("NameBR2", "SurnameBR2", 99);
+        Reader bookReader1 = new Reader("NameReader1", "SurnameReader1", 1);
+        Reader bookReader2 = new Reader("NameReader2", "SurnameReader2", 2);
 
 
         library.registerReader(bookReader1);
